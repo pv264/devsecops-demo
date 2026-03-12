021ad013345b6dbb4306439d6e5bb7ea55f22235
pv264 <117143247+pv264@users.noreply.github.com>
2026-03-12 12:48:21 +0530

    Update Application.java

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @GetMapping("/")
     public String home() {
-        return "DevSecOps Demo Application Running@2!";
+        return "Ti-DevSecOps Demo Application Running!";
     }
 
     public static void main(String[] args) {
